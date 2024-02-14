5f8bcfc36239ff428f2f4d0512a26da261347cde
druxski <160080798+druxski@users.noreply.github.com>
2024-02-15 00:56:49 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ export default function Page() {
               onClick={handleNoClick}
               className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
-              {noCount === 0 ? "Nie" : getNoButtonText()}
+              {noCount === 0 ? "No" : getNoButtonText()}
             </button>
           </div>
         </>
